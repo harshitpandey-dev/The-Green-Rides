d517a2d1aea1aa2a6d8919fc44c3ba7d3dee8761
Harshit <82134456+harshitpandey-dev@users.noreply.github.com>
2025-08-17 19:56:05 +0530

    Renamed frontend to web-frontend

--- backend/models/Rental.js
+++ backend/models/Rental.js
@@ -3,9 +3,30 @@ const mongoose = require("mongoose");
 const rentalSchema = new mongoose.Schema({
   user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
   cycle: { type: mongoose.Schema.Types.ObjectId, ref: "Cycle", required: true },
-  startTime: { type: Date, default: Date.now },
-  endTime: { type: Date },
-  status: { type: String, enum: ["active", "completed"], default: "active" },
+  duration: { type: Number, required: true },
+  returnedAt: { type: Date },
+  rentedBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  returnedBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  rentedLocation: {
+    type: String,
+    enum: ["east_campus", "west_campus"],
+    required: true,
+  },
+  returnedLocation: {
+    type: String,
+    enum: ["east_campus", "west_campus"],
+  },
+  rating: { type: Number, required: true, min: 1, max: 5 },
+  comment: { type: String, required: false },
+  isActive: { type: boolean, default: true },
   createdAt: { type: Date, default: Date.now },
 });
 
